4ef8df0b95becd50b4a862237f2e6581edce0f07
Fajar <maulanafajaribrahim@gmail.com>
2025-09-30 18:52:17 +0700

    fix: remove unused code

--- routes/web.php
+++ routes/web.php
@@ -25,16 +25,14 @@ Route::middleware(['auth'])->group(function () {
         ]);
     })->name('dashboard');
 
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
     Route::middleware([AdminMiddleware::class])->group(function () {
         Route::resource('company', CompanyController::class)->except('show', 'edit');
         Route::resource('employee', EmployeeController::class)->except('show', 'edit');
     });
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
